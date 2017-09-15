5c668bb36f7ca3d3a8bb2a678a68d46ac9fb1074
Jinchao Yang <jinchao2000@gmail.com>
2017-09-14 21:15:52 -0400

    Added more cases (commented). Changed logo.

--- GameOfLife/Init.cpp
+++ GameOfLife/Init.cpp
@@ -5,19 +5,36 @@ int main()
 {
     Gui::Builder builder;
 
-    builder.setDimensions(20, 20, 20);
-    
-    builder.setLiveRule(3, 7);
-    builder.setBirthRule(6, 6);
+    builder.setDimensions(30, 30, 30);
 
-    builder.setModularBoard(1);
+    builder.setLiveRule(2, 3);
+    builder.setBirthRule(4, 5);
 
-    //builder.setInitalState("ox\toox\txxx\nox\toox\txxx"); // glider 5766
+    builder.setModularBoard(0);
+
+    // builder.setInitalState("ox\toox\txxx\nox\toox\txxx"); // glider 5766
     // builder.setInitalState("xx\txx\txx"); //oscilator 5766
-    //builder.setInitalState("\tox\nox\txxx\tox\n\tox"); // blinker 5545
+    // builder.setInitalState("\tox\nox\txxx\tox\n\tox"); // blinker 5545
 
-    //builder.setInitalState("xx\noxx\nox");
-    builder.setTexture("C:/Users/jinch/Desktop/wpi.png");
+    // builder.setInitalState("xx\noxx\nox");
+
+    //builder.setInitalState(
+    //    "ooxxoooooxx\n"
+    //    "oooxxoooxx\n"
+    //    "xooxoxoxoxoox\n"
+    //    "xxxoxxoxxoxxx\n"
+    //    "oxoxoxoxoxox\n"
+    //    "ooxxxoooxxx\n"
+    //    "\n"
+    //    "ooxxxoooxxx\n"
+    //    "oxoxoxoxoxox\n"
+    //    "xxxoxxoxxoxxx\n"
+    //    "xooxoxoxoxoox\n"
+    //    "oooxxoooxx\n"
+    //    "ooxxoooooxx\n"
+    //); // blinker 2333 2d
+
+    builder.setTexture("mams.jpeg");
 
     builder.setDrawDelay(100);
 
